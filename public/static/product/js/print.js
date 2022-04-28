cdf672b88aa166a8f8b346bb1c74c875c94788bc
msrenly <1169656090@qq.com>
2022-04-28 18:26:07 +0800

    打印

--- public/static/product/js/print.js
+++ public/static/product/js/print.js
@@ -1,14 +1,14 @@
 //type=1 默认打印产品信息 type=2 打印物流信息
-function preview(parsedOrders,totalCopy=1,goods,goodsTwoCate,userName,businessName,type=1) {
+function preview(parsedOrders,totalCopy=1,goods,goodsTwoCate,userName,businessName,type=1,copysort=1,is_print_all=false,is_blank_lable=false) {
     // if(parsedOrders.first_name || parsedOrders.last_name){
     //     parsedOrders.nickname_print = parsedOrders.nickname+'('+parsedOrders.last_name+' '+parsedOrders.first_name+')'
     // }
-    init(parsedOrders,totalCopy,goods,goodsTwoCate,userName,businessName,type);
+    init(parsedOrders,totalCopy,goods,goodsTwoCate,userName,businessName,type,copysort,is_print_all,is_blank_lable);
     LODOP.PREVIEW();
 }
 //type=1 默认打印产品信息 type=2 打印物流信息
-function print(parsedOrders,totalCopy=1,goods,goodsTwoCate,userName,businessName,type=1) {
-    init(parsedOrders,totalCopy,goods,goodsTwoCate,userName,businessName);
+function print(parsedOrders,totalCopy=1,goods,goodsTwoCate,userName,businessName,type=1,copysort=1,is_print_all=false,is_blank_lable=false) {
+    init(parsedOrders,totalCopy,goods,goodsTwoCate,userName,businessName,type,copysort,is_print_all,is_blank_lable);
     LODOP.PRINT();
 }
 
@@ -17,7 +17,7 @@ var PRINT_MODE_SINGLE_LABEL_PER_PAGE = 'SINGLE_LABEL_PER_PAGE';
 var PRINT_MODE_THREE_LABEL_PER_PAGE = 'THREE_LABEL_PER_PAGE';
 var DEFAULT_PRINT_MODE = PRINT_MODE_SINGLE_LABEL_PER_PAGE;
 
-function init(order,totalCopy,goods,goodsTwoCate,businessName,userName,type) {
+function init(order,totalCopy,goods,goodsTwoCate,businessName,userName,type,copysort,is_print_all,is_blank_lable) {
     LODOP = getLodop();
     LODOP.PRINT_INIT("CityB2B-打印机预览");
     // LODOP.SET_PRINT_PAGESIZE(1, '3.6 in', '7 in', "");
@@ -27,17 +27,42 @@ function init(order,totalCopy,goods,goodsTwoCate,businessName,userName,type) {
         LODOP.SET_PRINT_PAGESIZE(1, '100mm', '100mm', "");
     }
     if (DEFAULT_PRINT_MODE === PRINT_MODE_SINGLE_LABEL_PER_PAGE) {
-        generateOrderPrint(order, totalCopy,goods,goodsTwoCate,businessName,userName,type);
+        generateOrderPrint(order, totalCopy,goods,goodsTwoCate,businessName,userName,type,copysort,is_print_all,is_blank_lable);
     } else if (DEFAULT_PRINT_MODE === PRINT_MODE_THREE_LABEL_PER_PAGE) {
-        generateOrderPrint2(order, totalCopy,goods,goodsTwoCate,businessName,userName,type);
+        generateOrderPrint2(order, totalCopy,goods,goodsTwoCate,businessName,userName,type,copysort,is_print_all,is_blank_lable);
     }
 }
 
 //single label per page
-function generateOrderPrint(order, copy,goods,goodsTwoCate,businessName,userName,type) {
-    for (var i = 0; i < copy; i++) {
-        order.boxLabel = i + 1 + " of " + copy;
-        addOnePage(order,goods,goodsTwoCate,businessName,userName,type);
+function generateOrderPrint(order, copy,goods,goodsTwoCate,businessName,userName,type,copysort,is_print_all,is_blank_lable) {
+    if(is_blank_lable){
+        if(is_print_all){
+            for (var i = 0; i < copy; i++) {
+                order.boxLabel = '__' + " of " + '__';
+                //order.boxLabel = i + 88 + " of " + 88;
+                addOnePage(order,goods,goodsTwoCate,businessName,userName,type,copysort,is_print_all,is_blank_lable);
+            }
+            return;
+        }else{
+            order.boxLabel = '__' + " of " + '__';
+            //order.boxLabel = i + 88 + " of " + 88;
+            addOnePage(order,goods,goodsTwoCate,businessName,userName,type);
+            return;
+        }
+    }else{
+        if(is_print_all){
+            for (var i = 0; i < copy; i++) {
+                order.boxLabel = i + 1 + " of " + copy;
+                //order.boxLabel = i + 88 + " of " + 88;
+                addOnePage(order,goods,goodsTwoCate,businessName,userName,type);
+            }
+            return;
+        }else{
+            order.boxLabel = copysort + " of " + copy;
+            //order.boxLabel = i + 88 + " of " + 88;
+            addOnePage(order,goods,goodsTwoCate,businessName,userName,type);
+            return;
+        }
     }
 }
 function addOnePage(order,goods,goodsTwoCate,businessName,userName,type) {
@@ -134,7 +159,6 @@ function labelTemplate(order,goods,goodsTwoCate,businessName,userName,type) {
         truck_name = order.truck_info.truck_name
         plate_number = order.truck_info.plate_number
     }
-    order.boxLabel = 1 + " of " + 2;
     if(type == 1){
         if(order.nickname.length>20 || goods.menu_en_name.length>20 || (goods.is_has_two_cate == 1 && goodsTwoCate.guige_name.length > 20)){
             if(order.nickname.length>25){
@@ -207,76 +231,72 @@ function labelTemplate(order,goods,goodsTwoCate,businessName,userName,type) {
         html+='	<label>Phone:</label>';
         html+='	<span style="float:right;">'+order.phone+'</span>';
         html+='</div>';
-
-
         html+='<hr>';
-
         html+='<label>Suppliers Name:</label>';
         html+='<div style="border: 0px solid black; padding: 5px">';
         //html+='	<span >'+order.logistic_suppliers_info+'</span>';
         html+='	<span >DNL FOOD   license  NO:P01417</span></div>';
         html+='<div style="border: 0px solid black; padding: 5px">	<span >TEL:93988222  0450599336 </span></div>';
         html+='<div style="border: 0px solid black; padding: 5px">	<span >ADD:30 Blaxland Ave, Thomastown VIC 3074</span></div>';
-
         html+='</div>';
 
-        html+='<p>offline|Delivery  CustId:'+order.userId+ '<br>CustName:<strong  style=\"width: 100%;font-size:25px;font-weight:bolder\" >'+order.nickname+'</strong></p>';
-        html+='<table style="width: 100%;font-size:30px;font-weight:bolder" cellspacing="0" cellpadding="0">';
-        html+='<tr>';
-        html+=	'<td style="font-size:18px ;border-width: 2px 1px 1px 2px;border-style:solid;text-align: left; ">&nbsp;&nbsp;'+new Date(order.logistic_delivery_date*1000).toLocaleDateString("en-US")+'</td>';
-        html+=	'<td style=" border-width: 1px 1px 1px 1px;border-style:solid;text-align: right;"><span style="font-size:18px">Drop No:&nbsp;&nbsp;</span>'+order.logistic_stop_No+'&nbsp;&nbsp;</td>';
-        html+='</tr>';
-        html+='<tr>';
-        html+=	'<td style="border-width: 1px 1px 1px 1px;border-style:solid;text-align: left;" >&nbsp;&nbsp;'+order.logistic_sequence_No+'</td>';
-        html+=	'<td style="border-width: 1px 1px 1px 1px;border-style:solid;text-align: right;" >&nbsp;<span style="font-size:18px">Box </span> '+order.boxLabel+'&nbsp;&nbsp;</td>';
-
-        html+='</tr>';
-        html+='</table>';
-
-        html+='<br>';
-        html+='	<label>Deliver Address:</label>';
-        html+='<br>';
-        html+='<div style="border: 3px solid black; padding: 5px">';
-        html+='	<span style="font-weight: bolder;">'+order.address + '</span>';
-
-        html+='</div>';
-        html+='<div>';
-        html+='	<label>Note:</label>  ';
-        html+='	<small>'+order.message_to_business+'</small>';
-        html+='</div>';
-        html+='<br><br>';
-        html+='<hr><br>';
-
-        html+='<div>';
-        html+='	<label>Order ID:</label>';
-        html+='	<span style="float:right;">'+order.orderId+'</span>';
-        html+='</div>';
-        html+='<div>';
-        html+='	<label>Name:</label>';
-        html+='	<span style="float:right;">'+order.first_name+' '+order.last_name+'</span>';
-        html+='</div>';
-        html+='<div>';
-        html+='	<label>Phone:</label>';
-        html+='	<span style="float:right;">'+order.phone+'</span>';
-        html+='</div>';
-
-        html+='<div>';
-        html+='	<label>Truck No:</label>';
-        html+='	<span style="float:right;">'+order.logistic_truck_No+'</span>';
-        html+='</div>';
-        html+='<div>';
-        html+='	<label>Stop No:</label>';
-        html+='	<span style="float:right;">'+order.logistic_stop_No+'</span>';
-        html+='</div>';
-        html+='<br><hr><br>';
-        html+='<div>';
-        html+='	<label>Suppliers Count:</label>';
-        html+='	<span style="float:right;">'+order.logistic_suppliers_count+'</span>';
-        html+='</div>';
-        html+='<label>Suppliers Name:</label>';
-        html+='<div style="border: 1px solid black; padding: 5px">';
-        html+='	<span >'+order.logistic_suppliers_info+'</span>';
-        html+='</div>';
+        // html+='<p>offline|Delivery  CustId:'+order.userId+ '<br>CustName:<strong  style=\"width: 100%;font-size:25px;font-weight:bolder\" >'+order.nickname+'</strong></p>';
+        // html+='<table style="width: 100%;font-size:30px;font-weight:bolder" cellspacing="0" cellpadding="0">';
+        // html+='<tr>';
+        // html+=	'<td style="font-size:18px ;border-width: 2px 1px 1px 2px;border-style:solid;text-align: left; ">&nbsp;&nbsp;'+new Date(order.logistic_delivery_date*1000).toLocaleDateString("en-US")+'</td>';
+        // html+=	'<td style=" border-width: 1px 1px 1px 1px;border-style:solid;text-align: right;"><span style="font-size:18px">Drop No:&nbsp;&nbsp;</span>'+order.logistic_stop_No+'&nbsp;&nbsp;</td>';
+        // html+='</tr>';
+        // html+='<tr>';
+        // html+=	'<td style="border-width: 1px 1px 1px 1px;border-style:solid;text-align: left;" >&nbsp;&nbsp;'+order.logistic_sequence_No+'</td>';
+        // html+=	'<td style="border-width: 1px 1px 1px 1px;border-style:solid;text-align: right;" >&nbsp;<span style="font-size:18px">Box </span> '+order.boxLabel+'&nbsp;&nbsp;</td>';
+        //
+        // html+='</tr>';
+        // html+='</table>';
+        //
+        // html+='<br>';
+        // html+='	<label>Deliver Address:</label>';
+        // html+='<br>';
+        // html+='<div style="border: 3px solid black; padding: 5px">';
+        // html+='	<span style="font-weight: bolder;">'+order.address + '</span>';
+        //
+        // html+='</div>';
+        // html+='<div>';
+        // html+='	<label>Note:</label>  ';
+        // html+='	<small>'+order.message_to_business+'</small>';
+        // html+='</div>';
+        // html+='<br><br>';
+        // html+='<hr><br>';
+        //
+        // html+='<div>';
+        // html+='	<label>Order ID:</label>';
+        // html+='	<span style="float:right;">'+order.orderId+'</span>';
+        // html+='</div>';
+        // html+='<div>';
+        // html+='	<label>Name:</label>';
+        // html+='	<span style="float:right;">'+order.first_name+' '+order.last_name+'</span>';
+        // html+='</div>';
+        // html+='<div>';
+        // html+='	<label>Phone:</label>';
+        // html+='	<span style="float:right;">'+order.phone+'</span>';
+        // html+='</div>';
+        //
+        // html+='<div>';
+        // html+='	<label>Truck No:</label>';
+        // html+='	<span style="float:right;">'+order.logistic_truck_No+'</span>';
+        // html+='</div>';
+        // html+='<div>';
+        // html+='	<label>Stop No:</label>';
+        // html+='	<span style="float:right;">'+order.logistic_stop_No+'</span>';
+        // html+='</div>';
+        // html+='<br><hr><br>';
+        // html+='<div>';
+        // html+='	<label>Suppliers Count:</label>';
+        // html+='	<span style="float:right;">'+order.logistic_suppliers_count+'</span>';
+        // html+='</div>';
+        // html+='<label>Suppliers Name:</label>';
+        // html+='<div style="border: 1px solid black; padding: 5px">';
+        // html+='	<span >'+order.logistic_suppliers_info+'</span>';
+        // html+='</div>';
     }
     return html;
 }
